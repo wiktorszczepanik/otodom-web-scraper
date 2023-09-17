0fe2a1e09baab1fbc76230de32427ecaf7de4510
Wiktor <126973859+wiktorszczepanik@users.noreply.github.com>
2023-09-17 16:08:48 +0200

    Add files via upload

---
+++ web_od/constans.py
@@ -0,0 +1,7 @@
+DRIVER = r";D:\web_driver"  # web driver type
+BASE_PATH = r"https://www.otodom.pl/"  # access with filters append
+STATIC_DELAY = True  # for extra vpn delay
+FILTERS_PATH = "filters/"  # directory for adding eststefilters
+DB_PATH = "db/"  # location of data base folder
+DB_NAME = "wynajem_mieszkania_warszawa"  # name of created data base
+CONTINUE_AFTER_BRAKE = False  # continue web scraping after program brake
